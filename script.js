5e35125fb2f65793df7bd7f6a84c573b635657db
Carlos <Silvacj90@gmail.com>
2022-03-16 15:19:20 -0400

    full knight animation sprite

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState = "idle";
+const dropdown = document.getElementById("animations");
+dropdown.addEventListener("change", function (e) {
+  playerState = e.target.value;
+});
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 1500);
@@ -7,19 +13,83 @@ const playerImage = new Image();
 playerImage.src = "/assets/Knight/Idle/idlesheet.png";
 const spriteWidth = 128;
 const spriteHeight = 128;
-let frameX = 0;
-let frameY = 0;
+
 let gameFrame = 0;
-let staggerFrames = 5;
+const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: 12,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 8,
+  },
+  {
+    name: "run_attack",
+    frames: 8,
+  },
+  {
+    name: "walk",
+    frames: 6,
+  },
+  {
+    name: "walk_attack",
+    frames: 6,
+  },
+  {
+    name: "attack",
+    frames: 5,
+  },
+  {
+    name: "attack_extra",
+    frames: 8,
+  },
+  {
+    name: "climb",
+    frames: 4,
+  },
+  {
+    name: "high_jump",
+    frames: 12,
+  },
+  {
+    name: "hurt",
+    frames: 4,
+  },
+  {
+    name: "push",
+    frames: 4,
+  },
+];
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  //ctx.fillRect(100, 50, 100, 100);
-  //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spriteAnimations[playerState].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y;
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth,
-    frameY * spriteHeight,
+    frameX,
+    frameY,
     spriteWidth,
     spriteHeight,
     400,
@@ -27,12 +97,8 @@ function animate() {
     800,
     800
   );
-  if (gameFrame % staggerFrames === 0) {
-    if (frameX < 11) frameX++;
-    else frameX = 0;
-  }
-  gameFrame++;
 
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
